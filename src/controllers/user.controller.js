4415232e8a3e0b2887773dbeb407d2f8ff1deb71
Ranjit Yadav <yadavranjit521@gmail.com>
2024-12-25 17:44:24 +0000

    feat(user): add auth middleware and logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -148,7 +148,26 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  // const { refreshToken } = req.cookies;
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { refreshToken: undefined },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
 export { registerUser, loginUser, logoutUser };
