1085d1804decf9a46cede2378c5ee17d2baaef22
Ranjit Yadav <yadavranjit521@gmail.com>
2025-01-19 14:24:22 +0000

    feat: Implement CRUD operations for posts.

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -6,19 +6,100 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPost = asyncHandler(async (req, res) => {
-  //TODO: create post
+  const { content } = req.body;
+
+  if (!content?.trim()) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const post = await Post.create({
+    content,
+    owner: req.user._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, post, "Post created successfully"));
 });
 
 const getUserPosts = asyncHandler(async (req, res) => {
-  // TODO: get user posts
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid userId");
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const posts = await Post.find({ owner: user._id });
+
+  return res.status(200).json(new ApiResponse(200, posts, "Posts fetched"));
 });
 
 const updatePost = asyncHandler(async (req, res) => {
-  //TODO: update post
+  const { postId } = req.params;
+
+  if (!isValidObjectId(postId)) {
+    throw new ApiError(400, "Invalid postId");
+  }
+
+  const { content } = req.body;
+
+  if (!content?.trim()) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const post = await Post.findById(postId);
+
+  if (!post) {
+    throw new ApiError(404, "Post not found");
+  }
+
+  if (post.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to update this post");
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(
+    postId,
+    {
+      content,
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedPost, "Post updated successfully"));
 });
 
 const deletePost = asyncHandler(async (req, res) => {
-  //TODO: delete post
+  const { postId } = req.params;
+
+  if (!isValidObjectId(postId)) {
+    throw new ApiError(400, "Invalid postId");
+  }
+
+  const post = await Post.findById(postId);
+
+  if (!post) {
+    throw new ApiError(404, "Post not found");
+  }
+
+  if (post.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to delete this post");
+  }
+
+  await Post.findByIdAndDelete(postId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Post deleted successfully"));
 });
 
 export { createPost, getUserPosts, updatePost, deletePost };
